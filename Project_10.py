56e479fa11e5a58ef46417230f03c085f21a2e74
PK <70811003+Kphanendra@users.noreply.github.com>
2026-03-25 23:57:24 +0530

    Number Utility Tool

---
+++ Project_10.py
@@ -0,0 +1,13 @@
+
+number = 1234567
+
+# 1.Convert number to binary, octal, hex
+print("Binary value        : {:b}".format(number))
+print("Octal value         : {:o}".format(number))
+print("Hex value           : {:x}".format(number))
+
+# 2.Format large numbers with commas
+print("Number with commas  : {:,}".format(number))
+
+# 3.Print scientific notation
+print("Scientific value    : {:e}".format(number))
